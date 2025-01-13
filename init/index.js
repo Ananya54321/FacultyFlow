aa9b27376b9d550e00859155158d157f28418c80
Ananya Pappula <116152722+Ananya54321@users.noreply.github.com>
2025-01-13 17:50:10 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 // const Subject = require("../models/Subject");
 const initSubData = require("./subdata.js");
 require("dotenv").config();
-const MONGO_URL =
-  process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {

a3a0cd346b4bf5d6b003ef0194a913bbe27bcb34
Ananya Pappula <116152722+Ananya54321@users.noreply.github.com>
2025-01-13 17:48:01 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // const Subject = require("../models/Subject");
 const initSubData = require("./subdata.js");
 const MONGO_URL =
-  "mongodb+srv://22b81a05y9:ananya521@clusterprojects.7x4tczd.mongodb.net/fwms";
+  process.env.MONGO_URL;
 
 main()
   .then(() => {
